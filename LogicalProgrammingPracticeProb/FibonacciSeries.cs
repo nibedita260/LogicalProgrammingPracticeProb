650afa96dd9e41fa52ce3cdf99620091aa2e5c4e
nibedita260 <deepi.nibedita260.com>
2021-09-22 16:06:56 +0530

    [Nibedita]:Added FibonacciSeries Iterative&Recursive Feature Implemented

---
+++ LogicalProgrammingPracticeProb/FibonacciSeries.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProgrammingPracticeProb
+{
+    class FibonacciSeries
+    {
+        //Fibonacci series -> 0 1 1 2 3 5 8 13 21 34 55 ....
+        public void PrintFibonacciSeries(int n)
+        {
+            int fib1 = 0, fib2 = 1, fibNum;
+            Console.WriteLine("Fibonacci Series of " + n + " are: ");
+            Console.Write(fib1 + " " + fib2 + " ");
+            for (int i = 2; i < n; i++)
+            {
+                fibNum = fib1 + fib2;
+                Console.Write(fibNum + " ");
+                fib1 = fib2;
+                fib2 = fibNum;
+            }
+            Console.WriteLine("  ");
+        }
+        //using Recursion
+        public void PrintFibonacciNum(int firstNumber,int secondNumber,int counter,int length)
+        {
+            if (counter < length)
+            {
+                Console.Write("{0} ", firstNumber);
+                PrintFibonacciNum(secondNumber,firstNumber+secondNumber,counter+1,length);
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ LogicalProgrammingPracticeProb/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LogicalProgrammingPracticeProb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int options;
+            bool isExit = false;
+            while (!isExit)
+            {
+                Console.WriteLine("Choose 1.FibonacciSeries");
+                options = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+                    switch (options)
+                    {
+                        case 1:
+                            FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                            fibonacciSeries.PrintFibonacciNum(0,1,1,15);
+                            break;
+                        default:
+                            isExit = true;
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Choose valid option");
+                }
+                
+            }
+        }
+                
+           
+        
+    }
+}
